00c5e35def93ad45ecd186957f5439feb52da481
cvvlm <michalsk7@gmail.com>
2022-10-24 00:44:32 +0200

    Add pokedex component
    Pokedex component added it takes data of pokemons and display it on page

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,19 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
+import { render } from '@testing-library/react';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Pokecard 
-        id={4}
-        name='Charmander'
-        type='fire'
-        exp={62}
-      
-      />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Pokedex />
+      </div>
+    );
+  }
 }
 
 export default App;
--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,18 +1,18 @@
 import React, {Component} from "react"
-import './Pokecard.css';
+
 
 const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
 class Pokecard extends Component {
     render() {
-        const {id, name, type, exp} = this.props
-        let imgSrc = `${POKE_API}${id}.png`
+        
+        let imgSrc = `${POKE_API}${this.props.id}.png`
         return(
             <div className="Pokecard">
-                <h1>{name}</h1>
-                <img src={imgSrc} alt={name} />
-                <p>Type: {type}</p>
-                <p>EXP: {exp}</p>
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc} alt={this.props.name} />
+                <p>Type: {this.props.type}</p>
+                <p>EXP: {this.props.exp}</p>
             </div>
         )
     }
