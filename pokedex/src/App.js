79e96935eb26f3e17dadf4cc06112931047b2b59
cvvlm <michalsk7@gmail.com>
2022-10-23 17:34:04 +0200

    files

---
+++ pokedex/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Pokecard from './Pokecard';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokecard 
+        id={4}
+        name='Charmander'
+        type='fire'
+        exp={62}
+      
+      />
+    </div>
+  );
+}
+
+export default App;
